8328cbc265f567785f932944a3a0e25e3ee0e147
Bryan Olandres <ozoneblacklight@outlook.com>
2022-12-14 22:47:30 +0800

    fixed layout on login page

--- pages/login.vue
+++ pages/login.vue
@@ -42,7 +42,7 @@
 </script>
 
 <template>
-  <div class="px-96">
+  <div class="px-80">
     <div class="card bg-base-300 shadow-xl">
       <div class="card-body">
         <div class="grid place-items-center">
@@ -57,52 +57,57 @@
           </div>
 
           <div class="w-96 h-96">
-            <Input label="Email Address" v-model="email" @keyup.enter="login" />
+            <div class="flex flex-col space-y-3">
+              <Input
+                label="Email Address"
+                v-model="email"
+                @keyup.enter="login" />
 
-            <Input
-              type="password"
-              label="Password"
-              v-model="password"
-              @keyup.enter="login" />
+              <Input
+                type="password"
+                label="Password"
+                v-model="password"
+                @keyup.enter="login" />
 
-            <button
-              class="btn mt-6 w-full"
-              :class="{ loading: isLoading }"
-              @click="login">
-              <span> Sign In </span>
-            </button>
+              <button
+                class="btn w-full"
+                :class="{ loading: isLoading }"
+                @click="login">
+                <span> Sign In </span>
+              </button>
 
-            <div class="grid place-items-center mt-3">
-              <div class="tooltip tooltip-bottom" data-tip="Aha ha...">
-                <a
-                  href="https://www.youtube.com/watch?v=8X_Ot0k4XJc"
-                  class="link text-sm">
-                  Forgot Password?
-                </a>
-              </div>
-            </div>
-
-            <!-- alert for failed logins -->
-            <Transition>
-              <div
-                class="alert alert-error shadow-lg mt-6"
-                v-if="isLoginFailed">
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="stroke-current flex-shrink-0 h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                  <span>Login failed. Please check your credentials.</span>
+              <div class="grid place-items-center mt-3">
+                <div class="tooltip tooltip-bottom" data-tip="Aha ha...">
+                  <a
+                    href="https://www.youtube.com/watch?v=8X_Ot0k4XJc"
+                    class="link text-sm">
+                    Forgot Password?
+                  </a>
                 </div>
               </div>
-            </Transition>
+
+              <!-- alert for failed logins -->
+              <Transition>
+                <div
+                  class="alert alert-error shadow-lg mt-6"
+                  v-if="isLoginFailed">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="stroke-current flex-shrink-0 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <span>Login failed. Please check your credentials.</span>
+                  </div>
+                </div>
+              </Transition>
+            </div>
           </div>
         </div>
       </div>
