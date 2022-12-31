7d4ed25f963ee0edcab473d95aa699bcdefe5d67
Bryan Olandres <ozoneblacklight@outlook.com>
2022-12-31 13:31:43 +0800

    changed from swap to just menu item for switching themes

--- components/navBar.vue
+++ components/navBar.vue
@@ -47,7 +47,7 @@
 
 <template>
   <div class="sticky top-0 z-20 bg-neutral bg-opacity-90 backdrop-blur w-full">
-    <div class="navbar px-40">
+    <div class="navbar px-32">
       <div class="navbar-start">
         <div class="prose lg:hidden">
           <h1 class="text-sky-400">
@@ -57,15 +57,56 @@
         </div>
       </div>
 
-      <div class="navbar-end gap-2">
+      <div class="navbar-end">
         <!-- user settings dwopdown -->
         <div class="dropdown dropdown-end">
           <label tabindex="0" class="btn btn-secondary">
             {{ currentUser.email }}
           </label>
+          <!--  -->
           <ul
             tabindex="0"
             class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+            <div class="tooltip tooltip-left" data-tip="y tho?">
+              <li v-if="currentTheme == 'dark'">
+                <a @click="changeTheme">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                  </svg>
+
+                  Light Mode
+                </a>
+              </li>
+            </div>
+
+            <li v-if="currentTheme == 'winter'">
+              <a @click="changeTheme">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                </svg>
+
+                my eyes, it hurts
+              </a>
+            </li>
+
             <li>
               <a @click="logout">
                 <svg
@@ -84,42 +125,6 @@
               </a>
             </li>
           </ul>
-        </div>
-
-        <!-- swap toggle to change themes -->
-        <div>
-          <label class="swap swap-rotate">
-            <!-- this hidden checkbox controls the state -->
-            <input type="checkbox" @click="changeTheme" />
-
-            <!-- sun icon -->
-            <svg
-              class="swap-off fill-current w-10 h-10"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-            </svg>
-
-            <!-- moon icon -->
-            <svg
-              class="swap-on fill-current w-10 h-10"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-            </svg>
-          </label>
         </div>
       </div>
     </div>
