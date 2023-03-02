1625216fc80693c9eb2c4af6701ff7df66f10d42
Bryan Olandres <ozoneblacklight@outlook.com>
2023-03-02 09:50:42 +0800

    removed unused var

--- pages/payon/resultcodes.vue
+++ pages/payon/resultcodes.vue
@@ -1,39 +1,39 @@
 <script setup>
-  import csvDownload from 'json-to-csv-export'
+import csvDownload from 'json-to-csv-export'
 
-  definePageMeta({
-    pageTitle: 'Kobeni | Result Codes',
-  })
+definePageMeta({
+  pageTitle: 'Kobeni | Result Codes',
+})
 
-  const currentUser = useState('currentUser')
+const currentUser = useState('currentUser')
 
-  // the one variable to rule them all
-  const result = reactive({
-    url: 'https://eu-test.oppwa.com/v1/resultcodes',
-    data: '',
-  })
+// the one variable to rule them all
+const result = reactive({
+  url: 'https://eu-test.oppwa.com/v1/resultcodes',
+  data: '',
+})
 
-  // fetch from ACI docs
-  const { data, pending, refresh } = await useFetch(result.url)
-  result.data = data.value
+// fetch from ACI docs
+const { data, refresh } = await useFetch(result.url)
+result.data = data.value
 
-  /**
-   * parse JSON into CSV and download to client
-   */
-  const exportCSV = () => {
-    try {
-      const dataToConvert = {
-        data: result.data.resultCodes,
-        filename: 'result_codes',
-        delimiter: ',',
-        headers: ['Code', 'Description'],
-      }
-      // dl!
-      csvDownload(dataToConvert)
-    } catch (error) {
-      console.error(error)
+/**
+ * parse JSON into CSV and download to client
+ */
+const exportCSV = () => {
+  try {
+    const dataToConvert = {
+      data: result.data.resultCodes,
+      filename: 'result_codes',
+      delimiter: ',',
+      headers: ['Code', 'Description'],
     }
+    // dl!
+    csvDownload(dataToConvert)
+  } catch (error) {
+    console.error(error)
   }
+}
 </script>
 
 <template>
@@ -48,16 +48,9 @@
       <div class="space-y-3">
         <div class="space-x-3" v-if="result.data">
           <button class="btn btn-primary gap-2" @click="exportCSV">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
                 d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5" />
             </svg>
 
@@ -65,16 +58,9 @@
           </button>
 
           <button class="btn btn-square" @click="refresh">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
                 d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
             </svg>
           </button>
