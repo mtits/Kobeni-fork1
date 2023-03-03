7c5d4a97d66493281b077429ddee63e1cc33495c
Bryan Olandres <ozoneblacklight@outlook.com>
2023-03-03 14:55:08 +0800

    recurring trx now updates reference ID

--- pages/payon/recurring.vue
+++ pages/payon/recurring.vue
@@ -1,127 +1,128 @@
 <script setup>
-  definePageMeta({
-    pageTitle: 'Kobeni | Recurring (REPEATED ONLY)',
+definePageMeta({
+  pageTitle: 'Kobeni | Recurring (REPEATED ONLY)',
+})
+
+const currentUser = useState('currentUser')
+
+//
+const mode = useState('mode')
+
+const modeText = computed(() => {
+  const subDomain = mode.value == 'Test' ? 'eu-test' : 'eu-prod'
+  return `https://${subDomain}.oppwa.com/v1/registrations/${registrationId.value}/payments`
+})
+
+//
+const registrationId = useState('registrationId')
+const accessToken = useState('accessToken')
+const entityId = useState('entityId')
+const referenceTransaction = useState('referenceTransaction')
+
+
+//
+const dataParameters = ref('')
+const defaultParameters = ref([
+  'amount=92.00',
+  'currency=USD',
+  'paymentType=DB',
+  'standingInstruction.mode=REPEATED',
+  'standingInstruction.type=RECURRING',
+  'standingInstruction.source=MIT',
+  'standingInstruction.initialTransactionId=REPLACE_ME',
+])
+
+//
+const responseData = ref('')
+const showLoading = ref(false)
+
+// all session data from here
+const sessionMode = ref('')
+const sessionRegId = ref('')
+const sessionAccessToken = ref('')
+const sessiondataParametersRecurring = ref('')
+const sessionEntityId = ref('')
+
+/**
+ * fetches data from the session and sets it to the local variables
+ */
+const getSessionData = async () => {
+  const { session, refresh } = await useSession()
+
+  await refresh()
+
+  sessionMode.value = session.value.mode
+  sessionRegId.value = session.value.registrationId
+  sessionAccessToken.value = session.value.accessToken
+  sessionEntityId.value = session.value.entityId
+  sessiondataParametersRecurring.value = session.value.dataParametersRecurring
+}
+
+/**
+ * set up session data on click of the submit button
+ */
+const setSessionData = async () => {
+  const { refresh, update } = await useSession()
+
+  await refresh()
+
+  await update({
+    mode: mode.value,
+    registrationId: registrationId.value,
+    accessToken: accessToken.value,
+    entityId: entityId.value,
+    dataParametersRecurring: dataParameters.value,
   })
+}
 
-  const currentUser = useState('currentUser')
+/**
+ * loads the session data as the main ui data
+ */
+const loadSessionData = () => {
+  mode.value = sessionMode.value
+  registrationId.value = sessionRegId.value
+  accessToken.value = sessionAccessToken.value
+  entityId.value = sessionEntityId.value
+  dataParameters.value = sessiondataParametersRecurring.value
+}
 
-  //
-  const mode = useState('mode')
-  const modeText = computed(() => {
-    if (mode.value == 'Test') {
-      return `https://eu-test.oppwa.com/v1/registrations/${registrationId.value}/payments`
-    } else {
-      return `https://eu-prod.oppwa.com/v1/registrations/${registrationId.value}/payments`
-    }
-  })
+/**
+ *
+ */
+const submit = async () => {
+  showLoading.value = true
+  responseData.value = ''
 
-  //
-  const registrationId = useState('registrationId')
-  const accessToken = useState('accessToken')
-  const entityId = useState('entityId')
+  await setSessionData()
+  await getSessionData()
 
-  //
-  const dataParameters = ref('')
-  const defaultParameters = ref([
-    'amount=92.00',
-    'currency=USD',
-    'paymentType=DB',
-    'standingInstruction.mode=REPEATED',
-    'standingInstruction.type=RECURRING',
-    'standingInstruction.source=MIT',
-    'standingInstruction.initialTransactionId=REPLACE_ME',
-  ])
-
-  //
-  const responseData = ref('')
-  const showLoading = ref(false)
-
-  // all session data from here
-  const sessionMode = ref('')
-  const sessionRegId = ref('')
-  const sessionAccessToken = ref('')
-  const sessiondataParametersRecurring = ref('')
-  const sessionEntityId = ref('')
-
-  /**
-   * fetches data from the session and sets it to the local variables
-   */
-  const getSessionData = async () => {
-    const { session, refresh } = await useSession()
-
-    await refresh()
-
-    sessionMode.value = session.value.mode
-    sessionRegId.value = session.value.registrationId
-    sessionAccessToken.value = session.value.accessToken
-    sessionEntityId.value = session.value.entityId
-    sessiondataParametersRecurring.value = session.value.dataParametersRecurring
-  }
-
-  /**
-   * set up session data on click of the submit button
-   */
-  const setSessionData = async () => {
-    const { refresh, update } = await useSession()
-
-    await refresh()
-
-    await update({
-      mode: mode.value,
-      registrationId: registrationId.value,
-      accessToken: accessToken.value,
-      entityId: entityId.value,
-      dataParametersRecurring: dataParameters.value,
+  try {
+    const { data } = await useFetch('/api/payon/recurring', {
+      method: 'post',
+      body: {
+        mode: mode.value,
+        registrationId: registrationId.value,
+        accessToken: accessToken.value,
+        dataParameters: `${textAreaToURLParams(
+          dataParameters.value
+        )}&entityId=${entityId.value}`,
+      },
     })
+
+    responseData.value = data.value
+    referenceTransaction.value = data.value.id
+  } catch (error) {
+    console.error(error)
+  } finally {
+    showLoading.value = false
   }
+}
 
-  /**
-   * loads the session data as the main ui data
-   */
-  const loadSessionData = () => {
-    mode.value = sessionMode.value
-    registrationId.value = sessionRegId.value
-    accessToken.value = sessionAccessToken.value
-    entityId.value = sessionEntityId.value
-    dataParameters.value = sessiondataParametersRecurring.value
-  }
+onMounted(async () => {
+  dataParameters.value = arrayToFormatter(defaultParameters.value, '\n')
 
-  /**
-   *
-   */
-  const submit = async () => {
-    showLoading.value = true
-    responseData.value = ''
-
-    await setSessionData()
-    await getSessionData()
-
-    try {
-      const { data } = await useFetch('/api/payon/recurring', {
-        method: 'post',
-        body: {
-          mode: mode.value,
-          registrationId: registrationId.value,
-          accessToken: accessToken.value,
-          dataParameters: `${textAreaToURLParams(
-            dataParameters.value
-          )}&entityId=${entityId.value}`,
-        },
-      })
-
-      responseData.value = data.value
-    } catch (error) {
-      console.error(error)
-    } finally {
-      showLoading.value = false
-    }
-  }
-
-  onMounted(async () => {
-    dataParameters.value = arrayToFormatter(defaultParameters.value, '\n')
-
-    await getSessionData()
-  })
+  await getSessionData()
+})
 </script>
 
 <template>
@@ -136,77 +137,42 @@
       <Input type="text" label="Registration ID" v-model="registrationId" />
 
       <!-- show endpoint -->
-      <InputReadOnly
-        label="Endpoint"
-        v-model="modeText"
-        :mode="mode"
-        :display-character-count="false"
+      <InputReadOnly label="Endpoint" v-model="modeText" :mode="mode" :display-character-count="false"
         @copy-content="copyString(modeText)" />
 
       <!-- Params -->
       <Textarea label="Data Parameters" v-model="dataParameters"></Textarea>
 
-      <button
-        class="btn btn-primary"
-        :class="{ loading: showLoading }"
-        @click="submit">
+      <button class="btn btn-primary" :class="{ loading: showLoading }" @click="submit">
         Submit
       </button>
 
       <Transition>
-        <div
-          class="tooltip tooltip-bottom z-20"
-          :data-tip="`${sessiondataParametersRecurring}\nRG=${sessionRegId}`">
-          <button
-            class="btn w-full"
-            @click="loadSessionData"
-            v-if="sessiondataParametersRecurring">
+        <div class="tooltip tooltip-bottom z-20" :data-tip="`${sessiondataParametersRecurring}\nRG=${sessionRegId}`">
+          <button class="btn w-full" @click="loadSessionData" v-if="sessiondataParametersRecurring">
             Load Previous Data
           </button>
         </div>
       </Transition>
 
       <Transition>
-        <Textareadisplayonly
-          label="Response Data"
-          :data="responseData"
-          v-if="responseData"></Textareadisplayonly>
+        <Textareadisplayonly label="Response Data" :data="responseData" v-if="responseData"></Textareadisplayonly>
       </Transition>
 
       <div class="btn-group">
-        <button
-          class="btn w-1/2 gap-2"
-          @click="copyEntireResponse(responseData)"
-          v-if="responseData">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
+        <button class="btn w-1/2 gap-2" @click="copyEntireResponse(responseData)" v-if="responseData">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-6 h-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+            <path stroke-linecap="round" stroke-linejoin="round"
               d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
           </svg>
           Response
         </button>
 
-        <button
-          class="btn w-1/2 gap-2"
-          @click="copyString(responseData.id)"
-          v-if="responseData.id">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
+        <button class="btn w-1/2 gap-2" @click="copyString(responseData.id)" v-if="responseData.id">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-6 h-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+            <path stroke-linecap="round" stroke-linejoin="round"
               d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
           </svg>
           Transaction ID
--- pages/payon/server-to-server.vue
+++ pages/payon/server-to-server.vue
@@ -1,141 +1,139 @@
 <script setup>
-  definePageMeta({
-    pageTitle: 'Kobeni | Server-to-Server',
+definePageMeta({
+  pageTitle: 'Kobeni | Server-to-Server',
+})
+
+const currentUser = useState('currentUser')
+
+//
+const mode = useState('mode')
+
+const modeText = computed(() => {
+  const subDomain = mode.value == 'Test' ? 'eu-test' : 'eu-prod'
+  return `https://${subDomain}.oppwa.com/v1/payments`
+})
+
+//
+const accessToken = useState('accessToken')
+const entityId = useState('entityId')
+const referenceTransaction = useState('referenceTransaction')
+const registrationId = useState('registrationId')
+
+//
+const dataParameters = ref('')
+const defaultParameters = ref([
+  'amount=1.00',
+  'currency=USD',
+  'paymentType=PA',
+  'paymentBrand=VISA',
+  'card.number=4200000000000000',
+  'card.holder=Nicolas Cage',
+  'card.expiryMonth=05',
+  'card.expiryYear=2034',
+  'card.cvv=123',
+  'billing.city=South Jadyn',
+  'billing.country=US',
+  'billing.street1=645 Delmer Vista Suite 927',
+  'billing.postcode=15705-9357',
+  'customer.email=test@test.com',
+  'customer.givenName=John',
+  'customer.surname=Wick',
+])
+
+//
+const responseData = ref('')
+const showLoading = ref(false)
+
+// all session data from here
+const sessionMode = ref('')
+const sessionAccessToken = ref('')
+const sessiondataParametersServerToServer = ref('')
+const sessionEntityId = ref('')
+
+/**
+ * fetches data from the session and sets it to the local variables
+ */
+async function getSessionData() {
+  const { session, refresh } = await useSession()
+
+  await refresh()
+
+  sessionMode.value = session.value.mode
+  sessionAccessToken.value = session.value.accessToken
+  sessionEntityId.value = session.value.entityId
+  sessiondataParametersServerToServer.value =
+    session.value.dataParametersServerToServer
+}
+
+/**
+ * set up session data on click of the submit button
+ */
+const setSessionData = async () => {
+  const { refresh, update } = await useSession()
+
+  await refresh()
+
+  await update({
+    mode: mode.value,
+    accessToken: accessToken.value,
+    entityId: entityId.value,
+    dataParametersServerToServer: dataParameters.value,
   })
+}
 
-  const currentUser = useState('currentUser')
+/**
+ * loads the session data as the main ui data
+ */
+const loadSessionData = () => {
+  mode.value = sessionMode.value
+  accessToken.value = sessionAccessToken.value
+  entityId.value = sessionEntityId.value
+  dataParameters.value = sessiondataParametersServerToServer.value
+}
 
-  //
-  const mode = useState('mode')
-  const modeText = computed(() => {
-    if (mode.value == 'Test') {
-      return 'https://eu-test.oppwa.com/v1/payments'
-    } else {
-      return 'https://eu-prod.oppwa.com/v1/payments'
-    }
-  })
+/**
+ *
+ */
+const submit = async () => {
+  showLoading.value = true
+  responseData.value = ''
 
-  //
-  const accessToken = useState('accessToken')
-  const entityId = useState('entityId')
-  const referenceTransaction = useState('referenceTransaction')
-  const registrationId = useState('registrationId')
+  await setSessionData()
+  await getSessionData()
 
-  //
-  const dataParameters = ref('')
-  const defaultParameters = ref([
-    'amount=1.00',
-    'currency=USD',
-    'paymentType=PA',
-    'paymentBrand=VISA',
-    'card.number=4200000000000000',
-    'card.holder=Nicolas Cage',
-    'card.expiryMonth=05',
-    'card.expiryYear=2034',
-    'card.cvv=123',
-    'billing.city=South Jadyn',
-    'billing.country=US',
-    'billing.street1=645 Delmer Vista Suite 927',
-    'billing.postcode=15705-9357',
-    'customer.email=test@test.com',
-    'customer.givenName=John',
-    'customer.surname=Wick',
-  ])
-
-  //
-  const responseData = ref('')
-  const showLoading = ref(false)
-
-  // all session data from here
-  const sessionMode = ref('')
-  const sessionAccessToken = ref('')
-  const sessiondataParametersServerToServer = ref('')
-  const sessionEntityId = ref('')
-
-  /**
-   * fetches data from the session and sets it to the local variables
-   */
-  async function getSessionData() {
-    const { session, refresh } = await useSession()
-
-    await refresh()
-
-    sessionMode.value = session.value.mode
-    sessionAccessToken.value = session.value.accessToken
-    sessionEntityId.value = session.value.entityId
-    sessiondataParametersServerToServer.value =
-      session.value.dataParametersServerToServer
-  }
-
-  /**
-   * set up session data on click of the submit button
-   */
-  const setSessionData = async () => {
-    const { refresh, update } = await useSession()
-
-    await refresh()
-
-    await update({
-      mode: mode.value,
-      accessToken: accessToken.value,
-      entityId: entityId.value,
-      dataParametersServerToServer: dataParameters.value,
+  try {
+    const { data } = await useFetch('/api/payon/server-2-server', {
+      method: 'post',
+      body: {
+        mode: mode.value,
+        accessToken: accessToken.value,
+        dataParameters: `${textAreaToURLParams(
+          dataParameters.value
+        )}&entityId=${entityId.value}`,
+      },
     })
-  }
 
-  /**
-   * loads the session data as the main ui data
-   */
-  const loadSessionData = () => {
-    mode.value = sessionMode.value
-    accessToken.value = sessionAccessToken.value
-    entityId.value = sessionEntityId.value
-    dataParameters.value = sessiondataParametersServerToServer.value
-  }
+    responseData.value = data.value
+    referenceTransaction.value = responseData.value.id
 
-  /**
-   *
-   */
-  const submit = async () => {
-    showLoading.value = true
-    responseData.value = ''
-
-    await setSessionData()
-    await getSessionData()
-
-    try {
-      const { data } = await useFetch('/api/payon/server-2-server', {
-        method: 'post',
-        body: {
-          mode: mode.value,
-          accessToken: accessToken.value,
-          dataParameters: `${textAreaToURLParams(
-            dataParameters.value
-          )}&entityId=${entityId.value}`,
-        },
-      })
-
-      responseData.value = data.value
-      referenceTransaction.value = responseData.value.id
-
-      if (responseData.value.registrationId) {
-        registrationId.value = responseData.value.registrationId
-      }
-    } catch (error) {
-      console.error(error)
-    } finally {
-      showLoading.value = false
+    if (responseData.value.registrationId) {
+      registrationId.value = responseData.value.registrationId
     }
+  } catch (error) {
+    console.error(error)
+  } finally {
+    showLoading.value = false
   }
+}
 
-  /**
-   *
-   */
-  onMounted(async () => {
-    dataParameters.value = arrayToFormatter(defaultParameters.value, '\n')
+/**
+ *
+ */
+onMounted(async () => {
+  dataParameters.value = arrayToFormatter(defaultParameters.value, '\n')
 
-    await getSessionData()
-  })
+  await getSessionData()
+})
 </script>
 
 <template>
@@ -148,96 +146,52 @@
 
     <div class="flex flex-col space-y-3">
       <!-- show endpoint -->
-      <InputReadOnly
-        label="Endpoint"
-        v-model="modeText"
-        :mode="mode"
-        :display-character-count="false"
+      <InputReadOnly label="Endpoint" v-model="modeText" :mode="mode" :display-character-count="false"
         @copy-content="copyString(modeText)" />
 
       <!-- Params -->
       <Textarea label="Data Parameters" v-model="dataParameters"></Textarea>
 
-      <button
-        class="btn btn-primary"
-        :class="{ loading: showLoading }"
-        @click="submit">
+      <button class="btn btn-primary" :class="{ loading: showLoading }" @click="submit">
         Submit
       </button>
 
       <Transition>
-        <div
-          class="tooltip tooltip-bottom z-20"
-          :data-tip="sessiondataParametersServerToServer">
-          <button
-            class="btn w-full"
-            @click="loadSessionData"
-            v-if="sessiondataParametersServerToServer">
+        <div class="tooltip tooltip-bottom z-20" :data-tip="sessiondataParametersServerToServer">
+          <button class="btn w-full" @click="loadSessionData" v-if="sessiondataParametersServerToServer">
             Load Previous Data
           </button>
         </div>
       </Transition>
 
       <Transition>
-        <Textareadisplayonly
-          label="Response Data"
-          :data="responseData"
-          v-if="responseData"></Textareadisplayonly>
+        <Textareadisplayonly label="Response Data" :data="responseData" v-if="responseData"></Textareadisplayonly>
       </Transition>
 
       <div class="btn-group">
-        <button
-          class="btn w-1/3 gap-2"
-          @click="copyEntireResponse(responseData)"
-          v-if="responseData">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
+        <button class="btn w-1/3 gap-2" @click="copyEntireResponse(responseData)" v-if="responseData">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-6 h-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+            <path stroke-linecap="round" stroke-linejoin="round"
               d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
           </svg>
           Response
         </button>
 
-        <button
-          class="btn w-1/3 gap-2"
-          @click="copyString(responseData.id)"
-          v-if="responseData.id">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
+        <button class="btn w-1/3 gap-2" @click="copyString(responseData.id)" v-if="responseData.id">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-6 h-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+            <path stroke-linecap="round" stroke-linejoin="round"
               d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
           </svg>
           Transaction ID
         </button>
 
-        <button
-          class="btn w-1/3 gap-2"
-          @click="copyString(responseData.registrationId)"
+        <button class="btn w-1/3 gap-2" @click="copyString(responseData.registrationId)"
           v-if="responseData.registrationId">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-6 h-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+            <path stroke-linecap="round" stroke-linejoin="round"
               d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
           </svg>
           Registration ID
