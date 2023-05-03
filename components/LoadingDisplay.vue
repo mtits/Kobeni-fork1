e257e12ba5a5a4604e5aa044dc804bccc6d22db0
Bryan Olandres <ozoneblacklight@outlook.com>
2023-05-03 13:25:18 +0800

    put msg inside div

--- components/LoadingDisplay.vue
+++ components/LoadingDisplay.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col gap-2">
-    Loading page...
+    <div>
+      Loading page...
+    </div>
+
     <div class="bg-gray-500 w-full h-6 rounded-lg animate-pulse">
     </div>
 
