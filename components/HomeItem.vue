c23d903747bbfd8886817117c0984f5d168461ca
Bryan Olandres <ozoneblacklight@outlook.com>
2023-05-06 18:22:52 +0800

    added proper home page

---
+++ components/HomeItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+
+    navTo: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+
+<template>
+  <div class="bg-base-200 p-5 rounded-lg w-72 h-40 shadow transition hover:scale-105">
+    <NuxtLink :to="navTo">
+      <h1 class="text-xl font-bold text-sky-400 mb-1">{{ title }}</h1>
+      <p class="text-xs">
+        <slot></slot>
+      </p>
+    </NuxtLink>
+  </div>
+</template>
