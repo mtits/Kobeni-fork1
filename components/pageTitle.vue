b98838a4ec85eaf8a293c67b65c59fbffc5c3b5d
Bryan Olandres <ozoneblacklight@outlook.com>
2023-05-06 18:24:19 +0800

    reduced font size on subtitles

--- components/pageTitle.vue
+++ components/pageTitle.vue
@@ -10,7 +10,7 @@
 <template>
   <div class="mb-6">
     <span class="text-4xl font-bold text-sky-400">{{ title }}</span>
-    <p class="mt-2">
+    <p class="mt-2 text-sm">
       <slot></slot>
     </p>
   </div>
