ecea5cb98c8f0c4d3bd27f62e7081365c728dc06
Bryan Olandres <ozoneblacklight@outlook.com>
2023-05-27 16:07:10 +0800

    more icon updates

--- pages/paymentz/generateAuthToken.vue
+++ pages/paymentz/generateAuthToken.vue
@@ -61,11 +61,7 @@
     <div class="flex flex-col gap-3">
       <div class="alert shadow-lg">
         <div>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-            class="stroke-info flex-shrink-0 w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-          </svg>
+          <IconInfo />
           <span>
             The following data are directly taken from the
             <NuxtLink class="link" to="/paymentz/config"> Setup page </NuxtLink>
