f1a1d98af42090697d1cee589dd8020d2e8140ad
Bryan Olandres <ozoneblacklight@outlook.com>
2023-06-14 16:13:17 +0800

    added interface to ip fetching, re-enabled keepalive

--- composables/useUserIp.ts
+++ composables/useUserIp.ts
@@ -1,12 +1,16 @@
 export const useUserIp = async (): Promise<string> => {
-  const { data } = await useFetch('https://api.ipify.org', {
+  interface ipifyResult {
+    ip: string
+  }
+
+  const { data } = await useFetch<ipifyResult>('https://api.ipify.org', {
     method: 'get',
     query: {
       format: 'json',
     },
   })
 
-  console.info(`Current user IP: ${(data as any).value.ip} - ( ͡° ᴥ ͡°) i c u...`)
+  console.info(`Current user IP: ${data.value!.ip} - ( ͡° ᴥ ͡°) i c u...`)
 
-  return (data as any).value.ip
+  return data.value!.ip
 }
