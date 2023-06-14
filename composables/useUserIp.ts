6f9b82352ac9e26bbe607e72e522ef9a91133d3e
Bryan Olandres <ozoneblacklight@outlook.com>
2023-06-14 16:23:07 +0800

    added surrounded with try/catch for better error handling

--- composables/useUserIp.ts
+++ composables/useUserIp.ts
@@ -3,14 +3,21 @@ export const useUserIp = async (): Promise<string> => {
     ip: string
   }
 
-  const { data } = await useFetch<ipifyResult>('https://api.ipify.org', {
-    method: 'get',
-    query: {
-      format: 'json',
-    },
-  })
+  try {
+    const { data } = await useFetch<ipifyResult>('https://api.ipify.org', {
+      method: 'get',
+      query: {
+        format: 'json',
+      },
+    })
 
-  console.info(`Current user IP: ${data.value!.ip} - ( ͡° ᴥ ͡°) i c u...`)
+    console.info(`Current user IP: ${data.value!.ip} - ( ͡° ᴥ ͡°) i c u...`)
+    return data.value!.ip
 
-  return data.value!.ip
+    //
+  } catch (error) {
+    //
+    console.error(error)
+    return 'Unable to fetch IP from service "api.ipify.org"'
+  }
 }
