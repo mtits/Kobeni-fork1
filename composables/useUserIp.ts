b38b847e7e124e8b34480b293b79471ad637da43
Bryan Olandres <ozoneblacklight@outlook.com>
2023-06-14 17:04:45 +0800

    added colors to ip logging

--- composables/useUserIp.ts
+++ composables/useUserIp.ts
@@ -3,7 +3,17 @@ export const useUserIp = async (): Promise<string> => {
     ip: string
   }
 
+  const style =
+    'background-color: rgb(120 53 15); padding: 3px; border-radius: 0.5rem; color: white;'
+
+  const errorMsg = 'Unable to fetch IP from service "api.ipify.org"'
+
   try {
+    console.log(
+      "%cProceeding to invade the user's privacy by fetching their IP, this will be sent to the NSA",
+      style
+    )
+
     const { data } = await useFetch<ipifyResult>('https://api.ipify.org', {
       method: 'get',
       query: {
@@ -11,13 +21,20 @@ export const useUserIp = async (): Promise<string> => {
       },
     })
 
-    console.info(`Current user IP: ${data.value!.ip} - ( ͡° ᴥ ͡°) i c u...`)
-    return data.value!.ip
+    if (data.value) {
+      console.info(`${data.value.ip} - ( ͡° ᴥ ͡°) i c u`)
+      return data.value!.ip
+
+      //
+    } else {
+      console.error(errorMsg)
+      return errorMsg
+    }
 
     //
   } catch (error) {
     //
     console.error(error)
-    return 'Unable to fetch IP from service "api.ipify.org"'
+    return errorMsg
   }
 }
